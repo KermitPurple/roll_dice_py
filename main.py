cf30f4ebd8725e4310b14dfe753dab3b02f74787
Shane McDonough <shanemcdman@gmail.com>
2021-12-28 15:55:43 -0800

    Add d100 special case

--- main.py
+++ main.py
@@ -25,23 +25,35 @@ def roll_d100() -> int:
     '''
     return random.randrange(10, 101, 10)
 
+def roll_die(n: int) -> int:
+    '''
+    same as roll_std_die but has special case d100
+    :returns: the selected roll
+    '''
+    if n == 100:
+        return roll_d100()
+    return roll_std_die(n)
+
 def parse_dice_roll(dice_roll_string: str) -> int:
     f'''
     parse dice roll string
-    e.g. 10d10, 5d6, d20, 2,d100
+    e.g. 10d10, 5d6, d20, 2d100
     :returns: the roll of the dice
     '''
     match tuple(map(int, filter(lambda x: x, dice_roll_string.strip().lower().split('d')))):
         case [repetitions, faces]:
-            return sum((roll_std_die(faces) for _ in range(repetitions)))
+            return sum((roll_die(faces) for _ in range(repetitions)))
         case [faces]:
-            return roll_std_die(faces)
+            return roll_die(faces)
         case _:
             raise ValueError('Invalid string')
 
 def main():
     '''Driver Code'''
     sys.argv.pop(0)
+    if len(sys.argv) == 0:
+        print('pass in dice roll string\ne.g. 10d10, 5d6, d20, 2d100')
+        return
     for arg in sys.argv:
         try:
             result = parse_dice_roll(arg)
